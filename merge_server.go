a172033348ea270de36e9f65d849a9b531ff9dd6
libin <libin@wenchu.io>
2023-07-10 10:13:38 +0800

    server option supports

--- merge_server.go
+++ merge_server.go
@@ -6,31 +6,47 @@ import (
 	"github.com/gorilla/mux"
 	"net/http"
 	"net/url"
+	"time"
 )
+
+type Option struct {
+	MaxRequestSize int64 //max request body size, default 100MiB
+	PingInterval   int   //server ping interval, default 5 seconds
+}
 
 type MergeServer struct {
 	rpcServer *jsonrpc.RPCServer
 }
 
-func NewMergeServer(strNamespace string, handler interface{}) *MergeServer {
-	rpcServer := jsonrpc.NewServer()
+func NewMergeServer(strNamespace string, handler interface{}, options ...*Option) *MergeServer {
+	var so *Option
+	var opts []jsonrpc.ServerOption
+	for _, so = range options {
+		if so.MaxRequestSize > 0 {
+			opts = append(opts, jsonrpc.WithMaxRequestSize(so.MaxRequestSize))
+		}
+		if so.PingInterval > 0 {
+			opts = append(opts, jsonrpc.WithServerPingInterval(time.Duration(so.PingInterval)*time.Second))
+		}
+	}
+	rpcServer := jsonrpc.NewServer(opts...)
 	rpcServer.Register(strNamespace, handler)
 	return &MergeServer{
 		rpcServer: rpcServer,
 	}
 }
 
-//ServeHTTP http handler
+// ServeHTTP http handler
 func (m *MergeServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	m.rpcServer.ServeHTTP(w, r)
 }
 
-//AliasMethod alias method to new name
+// AliasMethod alias method to new name
 func (m *MergeServer) AliasMethod(alias, original string) {
 	m.rpcServer.AliasMethod(alias, original)
 }
 
-//ListenAndServe start a http server (NOTE: routine will be blocked)
+// ListenAndServe start a http server (NOTE: routine will be blocked)
 func (m *MergeServer) ListenAndServe(strUrl string) (err error) {
 	u, err := url.Parse(strUrl)
 	if err != nil {
