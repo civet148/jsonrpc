b7b82084782b40eaf6f1599bb51aef93ab7ba7b4
lory <civet148@outlook.com>
2023-12-19 10:06:35 +0800

    remove os arch panic code

--- os_arch.go
+++ os_arch.go
@@ -1,11 +1,9 @@
 package jsonrpc
 
-import "runtime"
-
 func init() {
-	switch runtime.GOARCH {
-	case "amd64":
-	default:
-		panic("GOARCH is not amd64")
-	}
+	//switch runtime.GOARCH {
+	//case "amd64":
+	//default:
+	//	panic("GOARCH is not amd64")
+	//}
 }
